4663be580e1a7df8b961e453a40f1291b6a1587a
Yihang <yhfeng25@gmail.com>
2026-02-02 17:43:09 +0800

    sdfghj

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print("Hello World")
+
+import flask
+from flask import Flask, render_template, request, redirect, url_for
+from datetime import datetime
+import os
+from pymongo import MongoClient
+from pymongo.errors import ConnectionFailure, OperationFailure
+import certifi
+import urllib.parse
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('homepage.html')
